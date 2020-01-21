6b4106fe01d13f4f48d8403af749c1c597240312
Andy Armstrong <andy@hexten.net>
2020-01-21 22:35:18 +0000

    Add direct FB interface.

--- bin/unicorn.js
+++ bin/unicorn.js
@@ -1,51 +1,118 @@
 "use strict";
 
-const { UnicornHD } = require("..");
+const { UnicornHD, UnicornHDCanvas } = require("..");
 
-function update(u, pt) {
-  const nx = pt.x + pt.dx;
-  const ny = pt.y + pt.dy;
-  if (nx < 0 || nx >= u.width) pt.dx = -pt.dx;
-  if (ny < 0 || ny >= u.width) pt.dy = -pt.dy;
-  pt.x += pt.dx;
-  pt.y += pt.dy;
+class PolyWobble {
+  constructor(opt) {
+    this.opt = Object.assign(
+      { colour: "red", points: 3, width: 16, height: 16, speed: 0.1 },
+      opt || {}
+    );
+
+    this.points = [];
+    for (let i = 0; i < this.opt.points; i++)
+      this.points.push({
+        x: Math.random() * this.opt.width,
+        y: Math.random() * this.opt.height,
+        dx: (Math.random() * 2 - 1) * this.opt.speed,
+        dy: (Math.random() * 2 - 1) * this.opt.speed
+      });
+  }
+
+  update() {
+    for (const pt of this.points) {
+      const nx = pt.x + pt.dx;
+      const ny = pt.y + pt.dy;
+      if (nx < 0 || nx >= this.opt.width) pt.dx = -pt.dx;
+      if (ny < 0 || ny >= this.opt.height) pt.dy = -pt.dy;
+      pt.x += pt.dx;
+      pt.y += pt.dy;
+    }
+  }
+
+  draw(ctx) {
+    ctx.save();
+
+    ctx.lineJoin = "round";
+    ctx.strokeStyle = this.opt.colour;
+
+    ctx.beginPath();
+
+    for (const [i, { x, y }] of this.points.entries())
+      i === 0 ? ctx.moveTo(x, y) : ctx.lineTo(x, y);
+
+    ctx.closePath();
+
+    ctx.stroke();
+    ctx.restore();
+  }
+}
+
+function makeCanvas() {
+  const u = new UnicornHDCanvas();
+  const shapes = [];
+
+  shapes.push(
+    new PolyWobble({ colour: "red" }),
+    new PolyWobble({ colour: "green" }),
+    new PolyWobble({ colour: "blue" })
+  );
+
+  const redraw = () => {
+    const cvs = u.canvas;
+    const ctx = cvs.getContext("2d");
+
+    ctx.clearRect(0, 0, cvs.width, cvs.height);
+
+    for (const s of shapes) {
+      s.update();
+      s.draw(ctx);
+    }
+  };
+
+  return { u, redraw, rate: 1000 / 25 };
+}
+
+function makeUnicorn() {
+  const u = new UnicornHD();
+
+  const perimeter = x => {
+    if (x < u.width) return [x, 0];
+    x -= u.width;
+    if (x < u.height - 1) return [u.width - 1, x + 1];
+    x -= u.height - 1;
+    if (x < u.width - 1) return [u.width - 2 - x, u.height - 1];
+    x -= u.width - 1;
+    if (x < u.height - 2) return [0, u.height - 2 - x];
+  };
+
+  let phase = 0;
+
+  const nextPos = () => {
+    const p = perimeter(phase++);
+    if (p) return p;
+    phase = 0;
+    return perimeter(phase);
+  };
+
+  const redraw = () => {
+    u.clear();
+    const [x0, y0] = nextPos();
+    const [x1, y1] = [u.width - x0, u.height - y0];
+    //    u.set(x, y, [255, 0, 0]);
+    u.line(x0, y0, x1, y1, [255, 0, 0]);
+    //    u.line(0, 0, 15, 15, [255, 255, 0]);
+    //    u.set(0, 0, [255, 0, 0]);
+    //    u.set(1, 0, [0, 255, 0]);
+    //    u.set(2, 0, [0, 0, 255]);
+  };
+  return { u, redraw, rate: 250 };
 }
 
 (async () => {
   try {
-    const u = new UnicornHD();
-    const p = [];
-
-    for (let i = 0; i < 3; i++)
-      p.push({
-        x: Math.random() * u.width,
-        y: Math.random() * u.height,
-        dx: (Math.random() * 2 - 1) * 0.1,
-        dy: (Math.random() * 2 - 1) * 0.1
-      });
-
-    const redraw = () => {
-      for (const pt of p) update(u, pt);
-
-      const cvs = u.canvas;
-      const ctx = cvs.getContext("2d");
-
-      ctx.save();
-
-      ctx.clearRect(0, 0, cvs.width, cvs.height);
-      //      ctx.antialias = "none";
-
-      ctx.strokeStyle = "green";
-
-      ctx.beginPath();
-
-      const lp = p[p.length - 1];
-      ctx.moveTo(lp.x, lp.y);
-      for (const pt of p) ctx.lineTo(pt.x, pt.y);
-
-      ctx.stroke();
-      ctx.restore();
-    };
+    //    const { u, redraw, rate } = makeCanvas();
+    const { u, redraw, rate } = makeUnicorn();
 
     u.on("frame", redraw)
       .on("dropped", () => console.log("Dropped frame!"))
@@ -53,8 +120,8 @@ function update(u, pt) {
         throw e;
       });
 
-    redraw();
-    u.start(1000 / 25);
+    // 25 FPS
+    u.start(rate);
   } catch (e) {
     console.log(e);
     process.exit(1);
--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 "use strict";
 
 module.exports = {
-  UnicornHD: require("./lib/unicorn-hd")
+  ...require("./lib/unicorn-hd")
 };
